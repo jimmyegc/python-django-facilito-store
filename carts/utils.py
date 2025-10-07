88cb0702f18626969144c08aa9c702230dd35a6a
Jimmy García <jimmyegc@gmail.com>
2025-10-06 20:52:01 -0600

    10062025

---
+++ carts/utils.py
@@ -0,0 +1,26 @@
+from .models import Cart
+
+def get_or_create_cart(request):
+    user = request.user if request.user.is_authenticated else None
+    cart_id = request.session.get('cart_id')  # <-- Este debe guardar el uuid
+    print("🛍 session cart_id:", cart_id)
+    
+    # Buscar carrito existente por UUID
+    cart = Cart.objects.filter(cart_id=cart_id).first()
+    print("📦 found cart:", cart)
+     
+    # Si no existe, crearlo
+    if cart is None:
+        cart = Cart.objects.create(user=user)
+        print("🆕 created new cart:", cart.cart_id)
+    
+    # Asociar el usuario si aún no está asignado
+    if user and cart.user is None:
+        cart.user = user
+        cart.save()
+    
+    # Guardar el UUID del carrito en la sesión
+    request.session['cart_id'] = cart.cart_id
+    print("💾 saved to session:", request.session['cart_id'])
+
+    return cart
---
+++ carts/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import redirect, render, get_object_or_404
+from .utils import get_or_create_cart
+from products.models import Product
+from .models import CartProducts
+
+def cart(request):
+    #Crear una sesión
+    #request.session['cart_id'] = '123' #Dic
+    # Obtener una sesión
+    #valor = request.session.get('cart_id')
+    #print(valor)
+    # Eliminar sessión
+    #request.session['card_id'] = None
+        
+    cart = get_or_create_cart(request)
+    print("cart", cart.products)
+    return render(request, 'carts/cart.html', {'cart': cart})
+
+def add(request):
+    cart = get_or_create_cart(request)
+    product = get_object_or_404(Product, pk=request.POST.get('product_id'))
+    quantity = int(request.POST.get('quantity', 1))
+    
+    #cart.products.add(product, through_defaults={
+    #    'quantity': quantity
+    #})
+    
+    cart_product = CartProducts.objects.create_or_update_quantity(cart=cart, product=product, quantity=quantity)
+    
+    return render(request, 'carts/add.html', { 
+        'quantity': quantity,
+        'cart_product': cart_product,
+        'product': product
+    })
+
+def remove(request):    
+    cart = get_or_create_cart(request)
+    product = get_object_or_404(Product, pk=request.POST.get('product_id'))
+    #Product.objects.get(pk=request.POST.get('product_id'))
+    
+    cart.products.remove(product)
+    return redirect('carts:cart')
